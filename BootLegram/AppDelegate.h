d3e81d8f9ab927acfa0e6bc478ae8717d6c484cc
Clara Kim <claraj@fb.com>
2020-07-06 18:56:29 -0400

    Initial Commit

---
+++ BootLegram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BootLegram
+//
+//  Created by Clara Kim on 7/6/20.
+//  Copyright © 2020 Clara Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BootLegram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BootLegram
+//
+//  Created by Clara Kim on 7/6/20.
+//  Copyright © 2020 Clara Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
