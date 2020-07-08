1a1178ce63e6c05980fe307a06350cb129c03ef1
Clara Kim <claraj@fb.com>
2020-07-08 19:47:37 -0400

    User can view a profile tab and set a profile picture (that is saved in Parse). The profile tab's timeline is not yet set to just the user's posts, however.

---
+++ BootLegram/View Controllers/ProfileViewController.m
@@ -0,0 +1,134 @@
+//
+//  ProfileViewController.m
+//  BootLegram
+//
+//  Created by Clara Kim on 7/8/20.
+//  Copyright © 2020 Clara Kim. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+#import "PostCell.h"
+#import "Post.h"
+@import Parse;
+
+@interface ProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray<Post *> *posts;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet PFImageView *profilePicView;
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    self.nameLabel.text = PFUser.currentUser.username;
+    [self loadProfilePicture];
+    
+    [self getPosts];
+}
+
+- (void) loadProfilePicture{
+    self.profilePicView.image = nil;
+    if(PFUser.currentUser[@"profileImage"]){
+        self.profilePicView.file = PFUser.currentUser[@"profileImage"];
+        [self.profilePicView loadInBackground];
+    }
+}
+
+- (void) getPosts{
+    // construct PFQuery
+    PFQuery *postQuery = [Post query];
+    [postQuery orderByDescending:@"createdAt"];
+    [postQuery includeKey:@"author"];
+    postQuery.limit = 20;
+
+    // fetch data asynchronously
+    [postQuery findObjectsInBackgroundWithBlock:^(NSArray<Post *> * _Nullable posts, NSError * _Nullable error) {
+        if (posts) {
+            // do something with the data fetched
+            self.posts = [posts mutableCopy];
+            [self.tableView reloadData];
+        }
+        else {
+            // handle error
+            NSLog(@"Error getting posts");
+        }
+    }];
+}
+
+- (IBAction)onTapChangeProfile:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    CGSize size = CGSizeMake(200, 200);
+    UIImage *resizedImage = [self resizeImage:editedImage withSize:size];
+    
+    [self loadProfilePicture];
+    
+    NSData *imageData = UIImagePNGRepresentation(resizedImage);
+    PFUser.currentUser[@"profileImage"] = [PFFileObject fileObjectWithName:@"profile_image.png" data:imageData];
+    [PFUser.currentUser saveInBackground];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.posts.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell"];
+    cell.post = self.posts[indexPath.row];
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
